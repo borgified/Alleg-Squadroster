167f4bd7b171f6f192b6a8fa37621101f89f234d
Fwiffo <borgified@gmail.com>
2012-10-23 17:24:44 -0700

    still working on it

--- lib/Alleg/Squadroster.pm
+++ lib/Alleg/Squadroster.pm
@@ -8,27 +8,97 @@ use LWP::Simple;
 
 use vars qw($VERSION);
 
-$VERSION = '0.01';
+$VERSION = '1.01';
 
 my @ISA = qw(Exporter);
 my @EXPORT = qw(list_squads list_red list_grey list_leadership);
 
 my $DEBUG=0;
 
-my $roster_content = get 'https://acss.alleg.net/Stats/SquadRoster.aspx';
+my $roster_content = get 'http://acss.alleg.net/Stats/SquadRoster.aspx';
 if(!defined($roster_content)){
 	die "couldnt fetch squad roster: $!";
 }
 
-my $token='<TD valign=\'top\' nowrap><B><U>';
+my $token='<thead>';
 
 my %data;
 
-my @squads= split(/$token/,$roster_content);
+sub list_squads {
+#returns all squads in a list in order of appearance (left->right)
+	my @squads = split(/$token/,$roster_content);
+	my @results;
 
-shift(@squads);
+	shift(@squads);
 
-foreach my $squad (@squads){
+	@squads = split (/\n/,"@squads");
+
+	foreach my $squad (@squads){
+		if($squad =~ /<th class="centerHeaders">(.*)<\/th>/){
+			push(@results,$1);
+		}
+	}
+
+	return @results;
+}
+
+my @squads = &list_squads;
+
+my @aspnet = split(/<div class="AspNet-GridView">/,$roster_content);
+shift @aspnet;
+my @valign = split(/<table>/,"@aspnet");
+shift @valign;
+
+my $count=0;
+foreach my $item (@valign){
+	#print "$item\n";
+	my @list=split(/\n/,$item);
+	my($token,$callsign,$status);
+
+	my $helper=0;
+
+	foreach my $item (@list){
+#		print "-------------------------\n";
+#		print "$item\n";
+#		print "-------------------------\n";
+
+		if($item=~/<td style="width: 10px; text-align: right;">(.*)<\/td>/){
+			$token=$1;
+			$helper++;
+		}
+		if($item=~/<td>(.*)<\/td>/){
+			$callsign=$1;
+			$helper++;
+		}
+		if($item=~/<tr class="(.*)">/){
+			$status=$1;
+		}
+		if($helper==2){
+				if($token eq '&nbsp;'){
+					$token = 'p';
+				}
+				print "$squads[$count] $token $callsign $status\n";
+
+				if(!exists($data{"$squads[$count]"}{'roster'}){
+					my @roster;
+					push(@roster,$callsign);
+					$data{"$squads[$count]"}{'roster'}=\@roster;
+				}else{
+					
+					
+
+
+
+
+				$helper=0;
+		}
+	}
+	$count++;
+}
+
+
+
+__END__
 
 #gotta remove all <b> </b> because when it shows up, it doesnt leave a space behind the
 #callsign which i use for pattern matching
